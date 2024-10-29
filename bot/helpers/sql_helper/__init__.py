c87523849764f9c7b4266a24ff3d3589a61e761f
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-10-29 23:57:25 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -1,25 +1,23 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from bot import DATABASE_URL, LOGGER
 
 
-from pymongo import MongoClient
-from bot import DATABASE_URL, LOGGER
+def start() -> scoped_session:
+  try:
+    engine = create_engine("mongodb+srv://premiumbot:premiumbot@cluster0.5siafyp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+  except ValueError:
+    LOGGER.error('Invalid DATABASE_URL : Exiting now.')
+    exit(1)
 
 
-def start():
-    try:
-        client = MongoClient("mongodb+srv://premiumbot:premiumbot@cluster0.5siafyp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        db_name = "mydatabase"  # Specify your database name here
-        db = client[db_name]  # Access the database
-        return db
-    except Exception as e:
-        LOGGER.error(f'Error connecting to the database: {e}')
-        exit(1)
-
-db = start()
-
+BASE = declarative_base()
+SESSION = start()
 
 
 
