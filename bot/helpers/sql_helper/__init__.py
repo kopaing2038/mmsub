25ecd1e531bd3e9d734ddb6a161a79575e1e1f43
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-10-29 23:56:30 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -7,16 +7,19 @@ from bot import DATABASE_URL, LOGGER
 from pymongo import MongoClient
 from bot import DATABASE_URL, LOGGER
 
+
 def start():
     try:
-        client = MongoClient(DATABASE_URL)
-        db = client.get_default_database()  # Get the default database
+        client = MongoClient("mongodb+srv://premiumbot:premiumbot@cluster0.5siafyp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        db_name = "mydatabase"  # Specify your database name here
+        db = client[db_name]  # Access the database
         return db
     except Exception as e:
         LOGGER.error(f'Error connecting to the database: {e}')
         exit(1)
 
 db = start()
+
 
 
 
